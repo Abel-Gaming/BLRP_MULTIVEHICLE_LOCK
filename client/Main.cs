b9f6e0808181812e0e7f193a9dcc6899412621ed
Abel Gaming <lcpdpolice4939@gmail.com>
2022-04-03 01:09:35 -0400

    Create Main.cs

---
+++ client/Main.cs
@@ -0,0 +1,19 @@
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using CitizenFX.Core.UI;
+using System.Threading.Tasks;
+
+namespace BLRP_MULTIVEHICLE_LOCK
+{
+    public class Main : BaseScript
+    {
+        public static string Author = "Abel Gaming";
+        public static string ModName = "Multi-Vehicle Lock";
+        public static string Version = "1.0";
+
+        public Main()
+        {
+            
+        }
+    }
+}
